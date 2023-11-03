e71f623aa9933cb77a64c9cb180aecdd9d42488e
Krystian <krystianpiatkowski92@gmail.com>
2023-11-03 15:12:17 +0100

    backup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,29 +3,29 @@ module.exports = {
   content: ["./**/*.liquid"],
   theme: {
     extend: {
-			colors: {
-				accent: '#008060',
-				textColor: '#1d1d1d'
-			},
-			container: {
-				center: true,
-				padding: '20px'
-			},
-			animation: {
-        marquee: 'marquee 25s linear infinite',
-        marquee2: 'marquee2 25s linear infinite',
+      colors: {
+        accent: "#008060",
+        textColor: "#1d1d1d",
+      },
+      container: {
+        center: true,
+        padding: "20px",
+      },
+      animation: {
+        marquee: "marquee 25s linear infinite",
+        marquee2: "marquee2 25s linear infinite",
       },
       keyframes: {
         marquee: {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-100%)' },
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
         },
         marquee2: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(0%)' },
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
         },
       },
-		},
+    },
   },
   plugins: [],
-}
+};
