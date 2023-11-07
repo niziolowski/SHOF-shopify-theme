32dc584e1a0abbe5cc881a685f04f10d4e7452f8
Krystian <krystianpiatkowski92@gmail.com>
2023-11-07 20:59:46 +0100

    Added product-gallery slider update on variant change

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import "swiper/css";
 import Alpine from "alpinejs";
 import collapse from "@alpinejs/collapse";
 
-import productGallery from "./productGallery";
+import { swiper, swiper2 } from "./productGallery";
 import mobileMenu from "./mobileMenu";
 import slider from "./slider";
 import imageWithTitle from "./imageWithTitle";
@@ -14,6 +14,9 @@ Alpine.data("mobileMenu", mobileMenu);
 Alpine.data("slider", slider);
 Alpine.data("imageWithTitle", imageWithTitle);
 Alpine.data("filters", filters);
+
+// Product gallery global object
+window.swiper = swiper2;
 
 window.Alpine = Alpine;
 
