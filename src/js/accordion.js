c1fdacb3c8420d1723077c3d5280f97fbbdff80d
Krystian <krystianpiatkowski92@gmail.com>
2023-11-08 13:01:21 +0100

    Added accordion data parsing from product.description

---
+++ src/js/accordion.js
@@ -0,0 +1,49 @@
+export default () => ({
+  activeAccordion: '',
+
+  initialize(data) {
+    if (typeof data === 'array') this.data = data;
+
+    if (typeof data === 'string') this.data = this.parseHTMLToStructuredData(data);
+
+    console.log(this.data);
+  },
+
+  // Manage active accordion
+  setActiveAccordion(id) {
+    this.activeAccordion = this.activeAccordion == id ? '' : id;
+  },
+
+  //
+  parseHTMLToStructuredData(htmlString) {
+    // Create an element to conveniently manipulate and traverse the provided HTML
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(htmlString, 'text/html');
+    const output = [];
+
+    // Use querySelectorAll to find all <h1> elements
+    const headers = Array.from(doc.querySelectorAll('h1'));
+
+    headers.forEach((header, index) => {
+      // Define an object to store the current title and content
+      const section = {
+        title: header.textContent.trim(),
+        content: '',
+      };
+
+      // Initialize a variable to keep track of the current node for iteration
+      let currentNode = header.nextElementSibling;
+
+      // While a next element exists and it is not another <h1>, concatenate its outerHTML to the content string
+      while (currentNode && currentNode.tagName !== 'H1') {
+        section.content += currentNode.outerHTML;
+        currentNode = currentNode.nextElementSibling;
+      }
+
+      // Push the section object to the output array
+      output.push(section);
+    });
+
+    return output;
+  },
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,21 @@
-import "swiper/css";
-import Alpine from "alpinejs";
-import collapse from "@alpinejs/collapse";
+import 'swiper/css';
+import Alpine from 'alpinejs';
+import collapse from '@alpinejs/collapse';
 
-import { swiper, swiper2 } from "./productGallery";
-import mobileMenu from "./mobileMenu";
-import slider from "./slider";
-import imageWithTitle from "./imageWithTitle";
-import filters from "./filters";
+import { swiper, swiper2 } from './productGallery';
+import mobileMenu from './mobileMenu';
+import slider from './slider';
+import imageWithTitle from './imageWithTitle';
+import filters from './filters';
+import accordion from './accordion';
 
 Alpine.plugin(collapse);
 
-Alpine.data("mobileMenu", mobileMenu);
-Alpine.data("slider", slider);
-Alpine.data("imageWithTitle", imageWithTitle);
-Alpine.data("filters", filters);
+Alpine.data('mobileMenu', mobileMenu);
+Alpine.data('slider', slider);
+Alpine.data('imageWithTitle', imageWithTitle);
+Alpine.data('filters', filters);
+Alpine.data('accordion', accordion);
 
 // Product gallery global object
 window.swiper = swiper2;
