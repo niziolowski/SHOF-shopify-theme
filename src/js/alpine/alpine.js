4acf9659f2f4dcb4841142b565d403d5b49c965a
Krystian <krystianpiatkowski92@gmail.com>
2023-11-23 18:20:41 +0100

    Cleaned up file structure

---
+++ src/js/alpine/alpine.js
@@ -0,0 +1,40 @@
+// Core Alpine and plugins
+import Alpine from 'alpinejs';
+import collapse from '@alpinejs/collapse';
+import focus from '@alpinejs/focus';
+import intersect from '@alpinejs/intersect';
+
+// Alpine components
+import header from './components/header';
+import slider from './components/slider';
+import imageWithTitle from './components/imageWithTitle';
+import filters from './components/filters';
+import accordion from './components/accordion';
+import cart from './components/cart';
+import search from './components/search';
+
+// Alpine directives
+import tooltip from './directives/tooltip';
+import reveal from './directives/reveal';
+
+// Alpine plugin setup
+Alpine.plugin(collapse);
+Alpine.plugin(focus);
+Alpine.plugin(intersect);
+
+// Alpine data setup
+Alpine.data('header', header);
+Alpine.data('slider', slider);
+Alpine.data('imageWithTitle', imageWithTitle);
+Alpine.data('filters', filters);
+Alpine.data('accordion', accordion);
+Alpine.data('cart', cart);
+Alpine.data('search', search);
+
+// Alpine directives
+Alpine.directive('tooltip', tooltip);
+Alpine.directive('reveal', reveal);
+
+window.Alpine = Alpine;
+
+Alpine.start();
--- src/js/app.js
+++ src/js/app.js
@@ -1,49 +1,11 @@
-import 'swiper/css';
-import Alpine from 'alpinejs';
-import collapse from '@alpinejs/collapse';
-import focus from '@alpinejs/focus';
-import intersect from '@alpinejs/intersect';
+import { swiper, swiper2 } from './alpine/components/productGallery.js'; // Swiper instance for product gallery
 
-import { swiper, swiper2 } from './productGallery';
-
-// Alpine components
-import header from './header';
-import slider from './slider';
-import imageWithTitle from './imageWithTitle';
-import filters from './filters';
-import accordion from './accordion';
-import cart from './cart';
-import search from './search';
-
-// Alpine directives
-import tooltip from './tooltip';
-import reveal from './directives/reveal';
-// Alpine plugins
-Alpine.plugin(collapse);
-Alpine.plugin(focus);
-Alpine.plugin(intersect);
-
-// Alpine data
-Alpine.data('header', header);
-Alpine.data('slider', slider);
-Alpine.data('imageWithTitle', imageWithTitle);
-Alpine.data('filters', filters);
-Alpine.data('accordion', accordion);
-Alpine.data('cart', cart);
-Alpine.data('search', search);
+import instaFeed from './instaFeed.js'; // A function for adding reveal effect to InstaFeed third-party widget
+import './alpine/alpine.js'; // AlpineJS setup
 
 // Product gallery global object (for slide manipulation)
 window.swiper = swiper2;
 
-window.Alpine = Alpine;
-
-// Alpine directives
-Alpine.directive('tooltip', tooltip);
-Alpine.directive('reveal', reveal);
-
-Alpine.start();
-
-import instaFeed from './instaFeed'; // For adding AlpineJS to the app elements
 window.addEventListener('load', () => {
   instaFeed();
 });
