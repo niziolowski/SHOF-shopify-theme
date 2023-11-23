28da61ec68db723e9eb8db896070e08738e77e7d
Krystian <krystianpiatkowski92@gmail.com>
2023-11-23 13:55:10 +0100

    Added reveal on scroll to collection products

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,8 @@ import focus from '@alpinejs/focus';
 import intersect from '@alpinejs/intersect';
 
 import { swiper, swiper2 } from './productGallery';
+
+// Alpine components
 import header from './header';
 import slider from './slider';
 import imageWithTitle from './imageWithTitle';
@@ -14,10 +16,12 @@ import cart from './cart';
 import tooltip from './tooltip';
 import search from './search';
 
+// Alpine plugins
 Alpine.plugin(collapse);
 Alpine.plugin(focus);
 Alpine.plugin(intersect);
 
+// Alpine data
 Alpine.data('header', header);
 Alpine.data('slider', slider);
 Alpine.data('imageWithTitle', imageWithTitle);
@@ -26,11 +30,12 @@ Alpine.data('accordion', accordion);
 Alpine.data('cart', cart);
 Alpine.data('search', search);
 
-// Product gallery global object
+// Product gallery global object (for slide manipulation)
 window.swiper = swiper2;
 
 window.Alpine = Alpine;
 
+// Alpine directives
 Alpine.directive('tooltip', tooltip);
 
 Alpine.start();
