599e8f8ccf8c96fdccaa5a88984f98cacb29f8ba
Krystian <krystianpiatkowski92@gmail.com>
2024-01-08 20:13:04 +0100

    fixed reviews slider bugs

--- src/js/alpine/components/slider.js
+++ src/js/alpine/components/slider.js
@@ -12,27 +12,34 @@ export default () => ({
     // Get slide nodes
     const slides = Array.from(sliderContainer.children);
     this.totalSlides = slides.length;
+
     // Find the highest slide and save it's index for reference
-    this.maxSlideIndex = slides.reduce((acc, cur, i) => {
-      if (cur.clientHeight > acc.clientHeight) {
-        return i;
-      } else {
-        return acc;
-      }
-    }, 1);
+    const biggestSlide = slides.reduce(
+      (acc, cur, i) => {
+        if (cur.clientHeight > acc.height) {
+          return { height: cur.clientHeight, index: i + 1 };
+        } else {
+          return acc;
+        }
+      },
+      { height: 0, index: 0 }
+    );
+
+    this.maxSlideIndex = biggestSlide.index;
+
+    console.log(this.maxSlideIndex);
 
     // Adjust the slider height
     this.adjustContainerHeight();
     // Add event user windows resizing
-    window.addEventListener("resize", () => this.adjustContainerHeight());
+    window.addEventListener('resize', () => this.adjustContainerHeight());
   },
 
   adjustContainerHeight() {
     // Get slider container
     const sliderContainer = this.$refs.sliderContainer;
     // Get height from the biggest slide
-    const maxSlideHeight =
-      this.$refs[`slide-${this.maxSlideIndex}`].clientHeight;
+    const maxSlideHeight = this.$refs[`slide-${this.maxSlideIndex}`].clientHeight;
     // Adjust container height
     sliderContainer.style.height = `${maxSlideHeight}px`;
 
@@ -51,7 +58,7 @@ export default () => ({
     for (let i = 1; i <= this.totalSlides; i++) {
       if (i != this.currentIndex) {
         const slide = this.$refs[`slide-${i}`];
-        slide.classList.add("hide");
+        slide.classList.add('hide');
       }
     }
   },
@@ -66,23 +73,11 @@ export default () => ({
     const previousSlide = this.$refs[`slide-${this.currentIndex}`];
     const nextSlide = this.$refs[`slide-${nextIndex}`];
 
-    nextSlide.classList.remove(
-      "slide-in-left",
-      "slide-in-right",
-      "slide-out-left",
-      "slide-out-right",
-      "hide"
-    );
+    nextSlide.classList.remove('slide-in-left', 'slide-in-right', 'slide-out-left', 'slide-out-right', 'hide');
     void nextSlide.offsetWidth; // Reset animation
     nextSlide.classList.add(`slide-in-${direction}`);
 
-    previousSlide.classList.remove(
-      "slide-in-left",
-      "slide-in-right",
-      "slide-out-left",
-      "slide-out-right",
-      "hide"
-    );
+    previousSlide.classList.remove('slide-in-left', 'slide-in-right', 'slide-out-left', 'slide-out-right', 'hide');
     void previousSlide.offsetWidth; // Reset animation
     previousSlide.classList.add(`slide-out-${direction}`);
   },
@@ -106,24 +101,23 @@ export default () => ({
   nextSlide() {
     this.resetInterval();
     let index = (this.currentIndex % this.totalSlides) + 1;
-    this.transition("right", index);
+    this.transition('right', index);
     this.currentIndex = index;
   },
   previousSlide() {
     this.resetInterval();
-    let index =
-      ((this.currentIndex - 2 + this.totalSlides) % this.totalSlides) + 1;
-    this.transition("left", index);
+    let index = ((this.currentIndex - 2 + this.totalSlides) % this.totalSlides) + 1;
+    this.transition('left', index);
     this.currentIndex = index;
   },
   goToSlide(index) {
     this.resetInterval();
     if (index > this.currentIndex) {
-      this.transition("right", index);
+      this.transition('right', index);
       this.currentIndex = index;
     }
     if (index < this.currentIndex) {
-      this.transition("left", index);
+      this.transition('left', index);
       this.currentIndex = index;
     }
   },
