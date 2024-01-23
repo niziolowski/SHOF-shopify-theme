c4ee86168ec0b2b574314c491995528ab85d7d8c
Krystian <krystianpiatkowski92@gmail.com>
2024-01-23 13:05:23 +0100

    Refactored mobile-menu for better UX

--- src/js/alpine/components/header.js
+++ src/js/alpine/components/header.js
@@ -1,7 +1,18 @@
 export default () => ({
   isMobileOpen: false,
   isSearchOpen: false,
+  currentSubmenu: null,
 
+  toggleSubmenu(menu) {
+    // If the submenu is opened and user click on it again, go to category
+    if (menu === this.currentSubmenu) return;
+
+    // If the submenu is closed, prevent redirect and set submenu as current
+    this.$event.preventDefault();
+    this.currentSubmenu = menu;
+  },
+
+  // Mobile menu
   toggleMenu() {
     this.isMobileOpen = !this.isMobileOpen;
     if (this.isMobileOpen) {
